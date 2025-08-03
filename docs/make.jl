903df7276cabd6f769dd6a5a14051a4ea6409964
ZenanH <zenan.huo@outlook.com>
2025-08-03 21:24:32 +0200

    🗑️ Comment out unused documentation sections in `make.jl`

--- docs/make.jl
+++ docs/make.jl
@@ -17,29 +17,29 @@ makedocs(
             "introduction/ecosystem.md",
             "introduction/tips.md",
         ],
-        "Interface" => Any[
-            "interface/structs.md",
-            "interface/backendagnostic.md",
-            "interface/kernelfunctions.md",
-            "interface/mpmprocess.md",
-            "interface/workflow.md",
-            "interface/imexport.md",
-        ],
-        "Tutorials" => Any[
-            "tutorials/example1.md",
-            "tutorials/example2.md",
-            "tutorials/example3.md"
-        ],
-        "Advanced Topics" => Any[
-            "advancedtopics/customkernel.md",
-            "advancedtopics/customconstitutive.md",
-            "advancedtopics/custommpmprocess.md",
-            "advancedtopics/customextension.md",
-            "advancedtopics/customfriction.md"
-        ],
-        "Useful Tools" => Any[
-            "utils/debug.md",
-        ]
+        # "Interface" => Any[
+        #     "interface/structs.md",
+        #     "interface/backendagnostic.md",
+        #     "interface/kernelfunctions.md",
+        #     "interface/mpmprocess.md",
+        #     "interface/workflow.md",
+        #     "interface/imexport.md",
+        # ],
+        # "Tutorials" => Any[
+        #     "tutorials/example1.md",
+        #     "tutorials/example2.md",
+        #     "tutorials/example3.md"
+        # ],
+        # "Advanced Topics" => Any[
+        #     "advancedtopics/customkernel.md",
+        #     "advancedtopics/customconstitutive.md",
+        #     "advancedtopics/custommpmprocess.md",
+        #     "advancedtopics/customextension.md",
+        #     "advancedtopics/customfriction.md"
+        # ],
+        # "Useful Tools" => Any[
+        #     "utils/debug.md",
+        # ]
     ],
     warnonly = [:missing_docs, :cross_references],
 )
